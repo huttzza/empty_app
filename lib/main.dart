e3f342c0214ace66954950e40eaa8d6cfe15fa08
huttzza <dobbie6174@gmail.com>
2021-11-03 00:41:17 +0900

    base view construct

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:empty_app/parking_deatil.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          //primarySwatch: purple //Colors.blue,
+          primaryColor: Color(0xFF613EEA),
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            secondary: Color(0xFF16FC72),
+          ),
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              bodyText1:
+                  TextStyle(fontSize: 16, fontWeight: FontWeight.normal))),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ParkingDetail();
+  }
+}
---
+++ lib/parking_deatil.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class ParkingDetail extends StatefulWidget {
+  ParkingDetail({Key? key}) : super(key: key);
+
+  @override
+  _ParkingDetailState createState() => _ParkingDetailState();
+}
+
+class _ParkingDetailState extends State<ParkingDetail> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0.0,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.clear,
+                    color: Colors.grey,
+                  ))
+            ]),
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: width * 0.03),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: width,
+                height: height * 0.08,
+                child: Container(
+                  //color: Colors.blue,
+                  child: Container(
+                    padding: EdgeInsets.all(height * 0.02),
+                    child: Text(
+                      "Sogang Univ.",
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor, //Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 3,
+                            blurRadius: 7,
+                            offset: Offset(0, 3))
+                      ]),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.07,
+                child: Container(
+                  child: Text(
+                    "35, Baekbeom-ro, Mapo-gu, Seoul",
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.06,
+              ),
+              Container(
+                child: SizedBox(
+                  width: width * 0.5,
+                  height: height * 0.07,
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.directions_car_filled,
+                            color: Colors.white, size: height * 0.05),
+                        Icon(Icons.directions_car_filled,
+                            color: Colors.white, size: height * 0.05),
+                        Icon(Icons.directions_car_filled,
+                            color: Colors.white, size: height * 0.05),
+                        Icon(Icons.directions_car_filled,
+                            color: Colors.white, size: height * 0.05),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              //Icon(Icons.directions_car_filled) //directions_car_filled_outlined
+            ],
+          ),
+        ));
+  }
+}
